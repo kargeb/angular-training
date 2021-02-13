5f83d511c006ed855329fc87911acf3d766b4a4e
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-02-13 16:49:41 +0100

    create main page from bootstrap template

---
+++ src/app/shop/components/product-list/product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { detailedProducts } from '../../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProductListComponent implements OnInit {
+  products = detailedProducts;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ProductComponent } from './components/product/product.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
-  declarations: [ShopComponent, ProductComponent],
+  declarations: [ShopComponent, ProductComponent, ProductListComponent],
   imports: [CommonModule, ShopRoutingModule],
   exports: [ShopComponent],
 })
