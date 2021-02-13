7aaafa516fc4ca7c8a7812e65911fa734f4a74de
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-02-13 16:58:43 +0100

    create separate component for nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShopModule } from './shop/shop.module';
 import { MainPageComponent } from './main-page/main-page.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, MainPageComponent],
+  declarations: [AppComponent, MainPageComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, ShopModule],
   providers: [],
   bootstrap: [AppComponent],
