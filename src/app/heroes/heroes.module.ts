fc193d6bb14c09588bef8f8893e19eed05c63e09
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-02-14 12:46:33 +0100

    import formModule

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [HeroesComponent],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, FormsModule],
 })
 export class HeroesModule {}
