5cae668bb19020f8c09982adac2227961d16e8fd
karol gebarowski <karol.gebarowski@gmail.com>
2021-02-17 16:49:59 +0100

    create pm component and module

---
+++ src/app/plural/pm/pm.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PmComponent } from './pm.component';
+
+
+
+@NgModule({
+  declarations: [PmComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PmModule { }
