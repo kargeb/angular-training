805f67f4e7ecb1f08a84223998cc5341a5f10a27
karol gebarowski <karol.gebarowski@gmail.com>
2021-02-18 11:14:10 +0100

    create course app until 8 chapter

--- src/app/plural/pm/pm.module.ts
+++ src/app/plural/pm/pm.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PmComponent } from './pm.component';
-
-
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star/star.component';
 
 @NgModule({
-  declarations: [PmComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    PmComponent,
+    ProductListComponent,
+    StarComponent,
+    ConvertToSpacesPipe,
+  ],
+  imports: [CommonModule, FormsModule],
 })
-export class PmModule { }
+export class PmModule {}
